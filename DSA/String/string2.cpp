227968d656e6bfb2c0f451b6aecd57730da4084e
Rakesh Roshan <roshanrakesh7362@gmail.com>
2023-01-27 05:23:50 +0530

    24th Jan, 2023

---
+++ DSA/String/string2.cpp
@@ -0,0 +1,32 @@
+// Program to check whether a string is palindrome or not
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    char str[100];
+    int i, length;
+    int flag = 0;    
+
+    cout << "Enter String: " << endl;
+    cin >> str;    
+
+    length = strlen(str);    
+
+    for (i = 0; i < length ; i++) {
+        if (str[i] != str[length-i-1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag) {
+        cout << " " << str << " is not a palindrome" << endl;
+    }    
+    else {
+        cout << " " << str << " is a palindrome" << endl;
+    }
+    return 0;
+
+}
